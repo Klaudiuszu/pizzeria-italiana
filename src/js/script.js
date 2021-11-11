db904dd27c20ec1a86e3a30963de61242a6eb271
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2021-11-11 20:27:07 +0100

    init Accordion function

--- src/js/script.js
+++ src/js/script.js
@@ -64,10 +64,11 @@
       thisProduct.renderInMenu();
 
       console.log(thisProduct);
+
+      thisProduct.renderInMenu();
     }
     renderInMenu() {
       const thisProduct = this;
-
       /* generate HTML based on template */
       const generatedHTML = templates.menuProduct(thisProduct.data);
       /* create element using utils.createElementFromHTML */
@@ -76,6 +77,29 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       /* add element to menu */
       menuContainer.appendChild(this.element); // KRZYSZTOF CZEMU THIS?
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      //const clickableTrigger = document.getElementsByClassName('product__header');
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      /* prevent default action for event */
+        event.preventDefault();
+        //console.log(clickableTrigger);
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct != thisProduct.element){
+          activeProduct.classList.remove('active');
+          /* toggle active class on thisProduct.element */
+          thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        }
+      });
     }
   }
   const app = {
