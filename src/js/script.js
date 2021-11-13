90bb27a2f066d665bf3ccee42258242c4d10dd40
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2021-11-13 23:21:38 +0100

    add initOrderForm

--- src/js/script.js
+++ src/js/script.js
@@ -66,6 +66,9 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
     }
     renderInMenu() {
@@ -120,8 +123,24 @@
       
       );
     }
-
     initOrderForm(){
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
       const thisProduct = this;
       console.log(thisProduct);
     }
@@ -130,7 +149,6 @@
       const thisProduct = this;
       console.log(thisProduct);
     }
-
   }
 
   
