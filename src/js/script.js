1a569b1d75a2e69e897f676ee5e00782be441db8
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2021-11-14 19:56:20 +0100

    add body processOrder function

--- src/js/script.js
+++ src/js/script.js
@@ -146,8 +146,33 @@
     }
   
     processOrder(){
+
       const thisProduct = this;
-      console.log(thisProduct);
+
+  // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+  const formData = utils.serializeFormToObject(thisProduct.form);
+  console.log('formData', formData);
+
+  // set price to default price
+  let price = thisProduct.data.price;
+
+  // for every category (param)...
+  for(let paramId in thisProduct.data.params) {
+    // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+    const param = thisProduct.data.params[paramId];
+    console.log(paramId, param);
+
+    // for every option in this category
+    for(let optionId in param.options) {
+      // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+      const option = param.options[optionId];
+      console.log(optionId, option);
+    }
+  }
+
+  // update calculated price in the HTML
+  thisProduct.priceElem.innerHTML = price;
+}
     }
   }
 
